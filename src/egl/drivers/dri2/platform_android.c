e36d214abb12f240fdac451789e7e0e7947e9479
aaaapai <89151134+aaaapai@users.noreply.github.com>
2025-08-30 09:48:31 +0800

    Update platform_android.c

--- src/egl/drivers/dri2/platform_android.c
+++ src/egl/drivers/dri2/platform_android.c
@@ -464,7 +464,7 @@ droid_get_window_size(struct dri2_egl_surface *dri2_surf, int *w, int *h) {
 static void
 update_buffer_size(struct dri2_egl_surface *dri2_surf)
 {
-   _eglLog(_EGL_WARNING, "update_buffer_size: dri2_surf->buffer %p", dri2_surf->buffer);
+   // _eglLog(_EGL_WARNING, "update_buffer_size: dri2_surf->buffer %p", dri2_surf->buffer);
    droid_get_window_size(dri2_surf, &dri2_surf->base.Width, &dri2_surf->base.Height);
    /*dri2_surf->base.Width = dri2_surf->buffer->width;
    dri2_surf->base.Height = dri2_surf->buffer->height;*/
@@ -1328,6 +1328,24 @@ dri2_initialize_android(_EGLDisplay *disp)
    struct dri2_egl_display *dri2_dpy = dri2_egl_display(disp);
    const char *err;
 
+   if (disp->Options.Zink) {
+        dri2_dpy->driver_name = strdup("zink");
+        dri2_dpy->loader_extensions = droid_kopper_image_loader_extensions;
+        dri2_dpy->fd_render_gpu = -1;
+        dri2_dpy->pure_swrast = true;
+        dri2_detect_swrast_kopper(disp);
+        if (!dri2_create_screen(disp)) {
+            err = "DRI2: Failed to create swrast screen";
+            goto cleanup;
+        }
+        device_opened = EGL_TRUE;
+   } else {
+        dri2_dpy->gralloc = u_gralloc_create(U_GRALLOC_TYPE_AUTO);
+        if (dri2_dpy->gralloc == NULL) {
+            err = "DRI2: failed to get gralloc";
+            goto cleanup;
+        }
+
    /*dri2_dpy->gralloc = u_gralloc_create(U_GRALLOC_TYPE_LIBDRM);
    if (dri2_dpy->gralloc == NULL) {
       err = "DRI2: failed to get gralloc";
@@ -1341,13 +1359,8 @@ dri2_initialize_android(_EGLDisplay *disp)
 
    if ((!device_opened && disp->Options.ForceSoftware) ||
        force_pure_swrast) {
-      if (disp->Options.Zink) {
-         dri2_dpy->driver_name = strdup("zink");
-         dri2_dpy->loader_extensions = droid_kopper_image_loader_extensions;
-      } else {
-         dri2_dpy->driver_name = strdup("swrast");
-         dri2_dpy->loader_extensions = droid_swrast_image_loader_extensions;
-      }
+      dri2_dpy->driver_name = strdup("swrast");
+      dri2_dpy->loader_extensions = droid_swrast_image_loader_extensions;
       dri2_dpy->fd_render_gpu = -1;
       dri2_dpy->pure_swrast = true;
       dri2_detect_swrast_kopper(disp);
@@ -1358,6 +1371,7 @@ dri2_initialize_android(_EGLDisplay *disp)
       }
 
       device_opened = EGL_TRUE;
+   }
    }
 
    if (!device_opened) {
