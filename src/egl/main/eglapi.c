2c98e7882cb851df73fd39ffbe81c50389e927fd
aaaapai <89151134+aaaapai@users.noreply.github.com>
2025-10-25 16:43:06 +0800

    Update eglapi.c

--- src/egl/main/eglapi.c
+++ src/egl/main/eglapi.c
@@ -1778,8 +1778,8 @@ eglBindAPI(EGLenum api)
 
    t = _eglGetCurrentThread();
 
-   if (!_eglIsApiValid(api))
-      RETURN_EGL_ERROR(NULL, EGL_BAD_PARAMETER, EGL_FALSE);
+   /*if (!_eglIsApiValid(api))
+      RETURN_EGL_ERROR(NULL, EGL_BAD_PARAMETER, EGL_FALSE);*/
 
    t->CurrentAPI = api;
 
