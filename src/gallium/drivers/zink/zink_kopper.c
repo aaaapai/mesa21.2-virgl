19f7460fda07c958e3c429c5b9dfbff960571549
aaaapai <89151134+aaaapai@users.noreply.github.com>
2025-11-22 19:23:59 +0800

    Update zink_kopper.c

--- src/gallium/drivers/zink/zink_kopper.c
+++ src/gallium/drivers/zink/zink_kopper.c
@@ -80,9 +80,6 @@ kopper_CreateSurface(struct zink_screen *screen, struct kopper_displaytarget *cd
     case VK_STRUCTURE_TYPE_ANDROID_SURFACE_CREATE_INFO_KHR: {
       VkAndroidSurfaceCreateInfoKHR *wdroid = (VkAndroidSurfaceCreateInfoKHR *)&cdt->info.bos;
       error = VKSCR(CreateAndroidSurfaceKHR)(screen->instance, wdroid, NULL, &surface);
-      if (error != VK_SUCCESS) {
-         error = vkCreateSurfaceOHOS(screen->instance, (const VkSurfaceCreateInfoOHOS*)&wdroid, NULL, &surface);
-      }
       break;
     }
 #endif
