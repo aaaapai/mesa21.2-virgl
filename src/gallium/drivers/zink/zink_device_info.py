186bc5d710c1cc86a2e986f425076f059027f3aa
aaaapai <89151134+aaaapai@users.noreply.github.com>
2025-11-22 13:55:55 +0800

    Update zink_device_info.py

--- src/gallium/drivers/zink/zink_device_info.py
+++ src/gallium/drivers/zink/zink_device_info.py
@@ -221,6 +221,8 @@ EXTENSIONS = [
         features=True,
         properties=True,
         conditions=["$feats.descriptorBindingPartiallyBound"]),
+    Extension("VK_KHR_android_surface"),
+    Extension("VK_KHR_OHOS_surface"),
 ]
 
 # constructor: Versions(device_version(major, minor, patch), struct_version(major, minor))
