b70d28eba7a739e2d9318f825e7d18dbcf05cb90
aaaapai <89151134+aaaapai@users.noreply.github.com>
2025-11-22 20:24:49 +0800

    Update zink_render_pass.c

--- src/gallium/drivers/zink/zink_render_pass.c
+++ src/gallium/drivers/zink/zink_render_pass.c
@@ -332,7 +332,7 @@ zink_create_render_pass(struct zink_screen *screen,
    if (!rp)
       goto fail;
 
-   if ((screen->vk_version >= VK_MAKE_VERSION(1,1,0)) || !(getenv("ZINK_USE_RENDER_PASS"))) {
+   if ((screen->vk_version >= VK_MAKE_VERSION(1,1,0)) && !(getenv("ZINK_USE_RENDER_PASS"))) {
       rp->render_pass = create_render_pass2(screen, state, pstate);
    } else {
       rp->render_pass = create_render_pass(screen, state, pstate);
